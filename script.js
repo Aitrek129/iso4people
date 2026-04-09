f14603fb0ccbb1d286a32317a8b3aeb9868d50cc
Aitrek129 <lyt.iarik@gmail.com>
2026-04-09 19:12:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,45 @@ function escapeHtml(str) {
     });
 }
 o
+[
+    {
+        "type": "official",
+        "name": "Ubuntu 22.04 LTS",
+        "description": "Один из самых популярных дистрибутивов для новичков и профессионалов. Стабильный, с большим сообществом.",
+        "url": "https://releases.ubuntu.com/22.04/ubuntu-22.04.3-desktop-amd64.iso",
+        "size": "4.8 GB",
+        "date": "2023-08-10"
+    },
+    {
+        "type": "official",
+        "name": "Fedora Workstation 38",
+        "description": "Современный дистрибутив с новейшим ПО. Отлично подходит для разработки и работы.",
+        "url": "https://download.fedoraproject.org/pub/fedora/linux/releases/38/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-38-1.6.iso",
+        "size": "2.1 GB",
+        "date": "2023-04-18"
+    },
+    {
+        "type": "official",
+        "name": "Linux Mint 21.2",
+        "description": "Дружелюбный дистрибутив на базе Ubuntu. Идеален для перехода с Windows.",
+        "url": "https://mirrors.kernel.org/linuxmint/stable/21.2/linuxmint-21.2-cinnamon-64bit.iso",
+        "size": "2.8 GB",
+        "date": "2023-07-15"
+    },
+    {
+        "type": "custom",
+        "name": "MyLinux v1.0 для разработки",
+        "filename": "mylinux-dev-1.0.iso",
+        "size": "2.3 GB",
+        "description": "Кастомная сборка для веб-разработки с предустановленными инструментами (VS Code, Docker, Node.js, Python).",
+        "date": "2025-03-15"
+    },
+    {
+        "type": "custom",
+        "name": "MyLinux v1.0 для игр",
+        "filename": "mylinux-gaming-1.0.iso",
+        "size": "3.1 GB",
+        "description": "Оптимизированная сборка для игр с драйверами NVIDIA, Steam, Lutris и Wine.",
+        "date": "2025-03-10"
+    }
+]
